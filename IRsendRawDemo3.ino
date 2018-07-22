1718059af5538a20ec7e43651dc806f4e84d1bfc
sanghkim <2shkim@gmail.com>
2018-07-22 19:24:35 +0900

    Delay fix version
    Due to switch and for loop makes delay function to work wrongfully. 
    So, I used three for loop for 2h, 3h and 3h40m.

--- IRsendRawDemo3.ino
+++ IRsendRawDemo3.ino
@@ -26,10 +26,8 @@ void setup()
 }
 
 void loop() {
-  for(int k = 0; k < 18 ; k++) // for 9 hours = 30min * 18
+  for(int k = 0; k < 3 ; k++) // for 2 hours (20min on /20min off)
   {
-    int Onmin =0;
-    int Offmin =0;
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
@@ -52,37 +50,9 @@ void loop() {
     delay(200);     
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     irsend.sendLG(0x8809801,28);  // Mode 2 - removing humidity
-  
-    ///////////////////////////////////////////////////////////////////////////////////////
-    //delay(10000);
-    switch (k){
-        case 0:
-        case 1:
-        case 2:      
-        case 3:        
-              Onmin = 15;
-              break;
+       
+    delay(1200000); // 20m*60*1000
 
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:         
-              Onmin = 12;             
-              break;
-
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-              Onmin = 10;
-              break;
-        default:
-              Onmin = 8;
-              break;
-    }// end of switch(k)
-    delay(Onmin*60*1000);
     ///////////////////////////////////////////////////////////////////////////////////////
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(200);     
@@ -96,39 +66,93 @@ void loop() {
     delay(200);     
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW   
     irsend.sendLG(0x88C0051,28); // POWER OFF
-  
-    ///////////////////////////////////////////////////////////////////////////////////////
-    //   delay(2000);
-//    switch (k){
-//        case 0:
-//        case 1:
-//        case 2:      
-//        case 3:        
-//              Offmin = 15;
-//              break;
-//
-//        case 4:
-//        case 5:
-//        case 6:
-//        case 7:
-//        case 8:
-//        case 9:        
-//              Offmin = 18;              
-//              break;
-//
-//        case 10:
-//        case 11:
-//        case 12:
-//        case 13:
-//        case 14:
-//        case 15:
-//              Offmin = 20;   
-//              break;
-//        default:
-//              Offmin = 22;
-//            break;
-//    }// end of switch(k)   
-    Offmin = 30-Onmin;
-    delay(Offmin * 60* 1000);
+    
+    delay(1200000); // 20m*60*1000           
   }
+  for(int k = 0; k < 6 ; k++) // for 3 hours (10min on /20min off)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    irsend.sendLG(0x8800347,28); // POWER ON
+    delay(5000);   
+    
+    ///////////////////////////////////////////////////////////////////////////////////////
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    irsend.sendLG(0x880B252,28); // Mode1 AI
+    delay(2000);      
+    
+    ///////////////////////////////////////////////////////////////////////////////////////
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);   
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    irsend.sendLG(0x8809801,28);  // Mode 2 - removing humidity
+       
+    delay(600000); // 10m*60*1000
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);   
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW   
+    irsend.sendLG(0x88C0051,28); // POWER OFF
+    
+    delay(1200000); // 20m*60*1000           
+  }  
+  for(int k = 0; k < 8 ; k++) // for 3 hours 40m(10min on /30min off)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    irsend.sendLG(0x8800347,28); // POWER ON
+    delay(5000);   
+    
+    ///////////////////////////////////////////////////////////////////////////////////////
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    irsend.sendLG(0x880B252,28); // Mode1 AI
+    delay(2000);      
+    
+    ///////////////////////////////////////////////////////////////////////////////////////
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);   
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    irsend.sendLG(0x8809801,28);  // Mode 2 - removing humidity
+       
+    delay(600000); // 10m*60*1000
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);   
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);     
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW   
+    irsend.sendLG(0x88C0051,28); // POWER OFF
+    
+    delay(1800000); // 30m*60*1000           
+  }    
 }
